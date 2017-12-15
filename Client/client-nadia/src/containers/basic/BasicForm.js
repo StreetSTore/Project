a1b1510b9c57856cdff0f4feb0be70038d7c7ff1
nadia <nadiamed1993@gmail.com>
2017-12-15 17:23:12 +0200

    with form

--- Client/client-nadia/src/containers/basic/BasicForm.js
+++ Client/client-nadia/src/containers/basic/BasicForm.js
@@ -3,35 +3,47 @@ import React, { Component } from "react";
 import * as writetolog  from "./formValidation.js";
 import ImageUploader from 'react-images-upload';
 
+
 // make a border in css+ centralize submit + centalize the all form
 
 class BasicForm extends Component {
+  constructor(props) {
+    super(props);
+    };
+
+  onDrop(picture) {
+    this.setState({pictures: this.state.pictures.concat(picture)});
+  }
+
   render() {
     return (
       <div>
-        <Form onSubmit={submittedValues => this.setState( { submittedValues } )}>
+        <Form>
           { formApi => (
-            <form onSubmit={formApi.submitForm} id="form2">
+            <form  id="form2">
               <label htmlFor="firstName">First name</label>
-            <Text field="firstName" id="firstName" /><br />
+            <Text field="firstName" name="fName" id="firstName" /><br />
               <label htmlFor="lastName">Last name</label>
-              <Text field="lastName" id="lastName" />
-            <RadioGroup field="role">
+            <Text field="lastName"  name="lName" id="lastName" />
+
+          <RadioGroup field="role" id="role" name="role">
                 { group => (
                   <div>
                   <label htmlFor="choose"><b>Choose:</b> </label><br />
                     <label htmlFor="customer" className="mr-2">I'm Customer</label>
-                    <Radio group={group} value="customer" id="customer" className="mr-3 d-inline-block" />
+                  <Radio group={group} value="customer"  className="mr-3 d-inline-block" />
                   <label htmlFor="buisness" className="mr-2">I'm Buisness Owner</label>
-                    <Radio group={group} value="buisness" id="buisness" className="d-inline-block" />
+                    <Radio group={group} value="buisness"  className="d-inline-block" />
                   </div>
                 )}
               </RadioGroup>
             <label htmlFor="mail">Mail:</label>
-          <Text field="mail" oninput={writetolog.writetolog()} id="mail" /><br />
+          <Text field="mail"  id="mail" /><br />
         <label htmlFor="password">Password:</label>
-      <input type="password"  password name="password"/><br /><br />
-          <button type="submit" className="mb-4 btn btn-primary">Submit</button>
+      <input type="password"  name="password" id="password"/><br /><br />
+
+          <button  onClick={writetolog.writetolog} className="mb-4 btn btn-primary">Submit</button>
+
 
             </form>
           )}
--- Client/client-nadia/src/containers/basic/formValidation.js
+++ Client/client-nadia/src/containers/basic/formValidation.js
@@ -1,4 +1,21 @@
 export function writetolog() {
-//alert("asdasd") ;
+
+  var fname=document.getElementById("firstName").value;
+  var lname=document.getElementById("lastName").value;
+  var mail=document.getElementById("mail").value;
+  var pass=document.getElementById("password").value;
+  var role=document.getElementById("firstName").value;
+
+  var data={
+   fname:fName,
+   lname:lName
+  };
+
+  var data_to_send=JSON.stringify(data);
+  var xhttp = new XMLHttpRequest();
+  xhttp.open('POST', 'https://localhost:443', true);
+  xhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  xhttp.send(data_to_send);
+
 return {};
 }
