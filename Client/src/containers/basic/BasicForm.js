47919bf6db67829280945ca899e308030166bf28
igorlevk <igorl3009@gmail.com>
2017-12-23 12:55:39 +0200

    Revert "Client merge"
    This reverts commit bf119715bb34edce7daf4ef043a717ef131a675c.

--- Client/src/containers/basic/BasicForm.js
+++ Client/src/containers/basic/BasicForm.js
@@ -1,105 +1,59 @@
-import React, {Component} from 'react';
-import TextView from './TextView.js';
-import update from 'immutability-helper';
-import {run, ruleRunner} from './ruleRunner.js'
-import {required, mustMatch, minLength,emailValidator} from './rules.js';
-import $ from 'jquery';
+import { Form, Text, Radio, RadioGroup, Select, Checkbox,TextArea } from 'react-form';
+import React, { Component } from "react";
+import * as writetolog  from "./formValidation.js";
 import ImageUploader from 'react-images-upload';
-import * as writetolog from "./sendForm.js";
-import {ReversedRadioButton,RadioGroup, RadioButton} from 'react-radio-buttons';
 
 
-const fieldValidations = [
-  ruleRunner("firstName", "First Name", required),
-  ruleRunner("lastName", "Last Name", required),
-  ruleRunner("emailAddress", "Email Address", required,emailValidator),
-  ruleRunner("password1", "Password", required, minLength(8)),
-  ruleRunner("password2", "Password Confirmation", mustMatch("password1", "Password"))
-
-];
+// make a border in css+ centralize submit + centalize the all form
 
 class BasicForm extends Component {
-
   constructor(props) {
     super(props);
-    this.handleFieldChanged = this.handleFieldChanged.bind(this);
-  this.handleSubmitClicked = this.handleSubmitClicked.bind(this);
-    this.errorFor = this.errorFor.bind(this);
-    this.state = {
-      showErrors: false,
-      validationErrors: {}
-    }
-  }
-
-  componentWillMount() {
-    // Run validations on initial state
-    this.setState({
-      validationErrors: run(this.state, fieldValidations)
-    });
-  }
-
-  errorFor(field) {
-    return this.state.validationErrors[field] || "";
-  }
-
-  handleFieldChanged(field) {
-    return(e) => {
-      let newState = update(this.state, {
-        [field]: {
-          $set: e.target.value
-        }
-      });
-      newState.validationErrors = run(newState, fieldValidations);
-      this.setState(newState);
     };
-  }
-
-  handleSubmitClicked() {
-    this.setState({showErrors: true});
-    if($.isEmptyObject(this.state.validationErrors) === false) {
-      return null;
-    }
-    else{
-
-      //return this.props.onCreateAccount(this.state);
-      writetolog.writetolog(this.state);
-      return null;
-    }
-
-  }
 
   onDrop(picture) {
     this.setState({pictures: this.state.pictures.concat(picture)});
   }
+  
+
+
   render() {
     return (
-    //  <div id="result"></div>
-    <div className="CreateAccount">
-      <form>
-      <h4>Create a New Account</h4><br/>
+      <div>
+		  <div id = "result">fsdfsdf</div>
+        <Form>
+          { formApi => (
+            <form  id="form2">
+              <label htmlFor="firstName">First name</label>
+            <Text field="firstName" name="fName" id="firstName" /><br />
+              <label htmlFor="lastName">Last name</label>
+            <Text field="lastName"  name="lName" id="lastName" />
 
-      <TextView placeholder="First Name" showError={this.state.showErrors} text={this.props.firstName} onFieldChanged={this.handleFieldChanged("firstName")} errorText={this.errorFor("firstName")} name="fName" /><br/>
-      <TextView placeholder="Last Name" showError={this.state.showErrors} text={this.props.firstName} onFieldChanged={this.handleFieldChanged("lastName")} errorText={this.errorFor("lastName")} name="lName" id="lastName"/><br/>
-      <TextView placeholder="Email Address" showError={this.state.showErrors} text={this.props.emailAddress} onFieldChanged={this.handleFieldChanged("emailAddress")} errorText={this.errorFor("emailAddress")} name="mail" id="mail" value="Email" /><br/>
-    <input id="sushirole" type="hidden" class="one"/>
-<b>Choose Who are you:</b>
-    <RadioGroup id="role" name="role" onChange={writetolog.changeradio} horizontal value="1">
-    <ReversedRadioButton value="1"  >
-      Customer
-    </ReversedRadioButton>
-    <ReversedRadioButton value="2"  >
-      Buisness
-    </ReversedRadioButton>
-  </RadioGroup><br/><br/>
-<TextView placeholder="Password" onChange={this.test} showError={this.state.showErrors} type="password" text={this.props.password1} onFieldChanged={this.handleFieldChanged("password1")} errorText={this.errorFor("password1")} name="password" id="password"/><br/>
-<TextView placeholder="Confirm Password" showError={this.state.showErrors} type="password"
-          text={this.props.password2} onFieldChanged={this.handleFieldChanged("password2")}
-          errorText={this.errorFor("password2")} />
-    <b>Upload Profile Pic:</b>
-  <ImageUploader id="pic" fileSizeError= " file size is too big" fileTypeError=" is not supported file extension" withIcon={true} buttonText='Choose images' onChange={this.onDrop} imgExtension={['.jpg', '.gif', '.png', '.gif']} maxFileSize={5242880}/>
-<input id="CreateAccountButton" type='button' className="mb-4 btn btn-primary" value="Create Account" onClick={this.handleSubmitClicked} ></input>
-  </form>
-  </div>);
-}
+          <RadioGroup field="role" id="role" name="role">
+                { group => (
+                  <div>
+                  <label htmlFor="choose"><b>Choose:</b> </label><br />
+                    <label htmlFor="customer" className="mr-2">I'm Customer</label>
+                  <Radio group={group} value="1"  className="mr-3 d-inline-block" />
+                  <label htmlFor="buisness" className="mr-2">I'm Buisness Owner</label>
+                <Radio group={group} value="2"  className="d-inline-block" />
+                  </div>
+                )}
+              </RadioGroup>
+            <label htmlFor="mail">Mail:</label>
+			<Text field="mail"  id="mail" /><br />
+			<label htmlFor="password">Password:</label>
+			<input type="password"  name="password" id="password"/><br /><br />
+	        <label htmlFor="img" >Upload Photo:</label>
+            <ImageUploader id="pic" withIcon={true} buttonText='Choose images' onChange={this.onDrop} imgExtension={['.jpg', '.gif', '.png', '.gif']} maxFileSize={5242880}/>
+          
+			<button  onClick={writetolog.writetolog} className="mb-4 btn btn-primary">Submit</button>
+
+            </form>
+          )}
+        </Form>
+      </div>
+    );
+  }
 }
 export default BasicForm;
